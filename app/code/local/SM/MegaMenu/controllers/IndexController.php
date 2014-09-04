b2819d5c1e3d51adebaf33f27c1c4c1c7be19ef6
luoi <hoanghh.vnu@gmail.com>
2014-09-04 14:08:50 +0700

    add Module megamenu

---
+++ app/code/local/SM/MegaMenu/controllers/IndexController.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: luoi
+ * Date: 9/4/14
+ * Time: 2:01 PM
+ */
+class SM_MegaMenu_IndexController extends Mage_Core_Controller_Front_Action{
+    public function indexAction(){
+        echo __METHOD__;
+    } // end indexAction
+} // end class
+// end file
